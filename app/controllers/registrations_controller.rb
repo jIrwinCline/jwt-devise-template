cc075cf169c1e221075b033181e3a7fb5ec79209
jirwincline <jci@pdx.edu>
2020-04-23 17:06:17 -0700

    fix devise create method to accept the necessary parameters and put them in the db as a user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,9 +2,17 @@ class RegistrationsController < Devise::RegistrationsController
     respond_to :json
   
     def create
-      build_resource(sign_up_params)
-  
+      puts "here!!"
+      puts params["user"]
+      puts params
+      build_resource(params["user"].permit(:user, :registration, :password, :password_confirmation, :email))
       resource.save
       render_resource(resource)
+    end
+
+    private
+
+    def resource_params
+      params.permit(:user, :registration, :password, :password_confirmation)
     end
   end
