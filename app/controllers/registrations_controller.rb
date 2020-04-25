705503d151831203940e04135acb680749055fea
jirwincline <jci@pdx.edu>
2020-04-24 18:04:15 -0700

    remove puts

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,9 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
     respond_to :json
   
     def create
-      puts "here!!"
-      puts params["user"]
-      puts params
       build_resource(params["user"].permit(:user, :registration, :password, :password_confirmation, :email))
       resource.save
       render_resource(resource)
